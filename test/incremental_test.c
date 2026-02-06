b9f2264fd8cdd9a769f346d4fd4fee2285e3a0a0
Boston_Brooks <pbrooks193@gmail.com>
2026-02-06 18:14:17 +1000

    Setting up build system

---
+++ test/incremental_test.c
@@ -0,0 +1,37 @@
+#include <stdio.h>
+#include <SFML/System.h>
+#include <SFML/Graphics.h>
+#include <SFML/Window.h>
+#include <pthread.h>
+
+void* graphics_thread(void* arg);
+int main(void)
+{
+    printf("Hello, World!\n");
+
+    pthread_t graphics_pthread;
+    pthread_create(&graphics_pthread, NULL, graphics_thread, NULL);
+
+    sfSleep(sfSeconds(5.f));
+
+    return 0;
+}
+
+void* graphics_thread(void* arg)
+{
+    sfTexture* splash_texture = sfTexture_createFromFile("./graphics/Splash.png", NULL);
+    sfSprite* splash_sprite = sfSprite_create();
+    sfSprite_setTexture(splash_sprite, splash_texture, sfTrue);
+
+    sfVideoMode mode;
+    mode.width = 720;
+    mode.height = 480;
+    mode.bitsPerPixel = 32;
+
+    sfRenderWindow* window = sfRenderWindow_create(mode, "early demo", sfResize | sfClose, NULL);
+    sfRenderWindow_drawSprite(window, splash_sprite, NULL);
+    sfRenderWindow_display(window);
+
+    sfSleep(sfSeconds(5.f));
+    return 0;
+}
