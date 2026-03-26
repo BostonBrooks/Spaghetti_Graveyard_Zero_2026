06455f1c9df5ee7a315d1c37da22110f59a1803a
boston-brooks <pbrooks193@gmail.com>
2026-03-26 16:14:08 +1000

    threaded queue test fails!

--- test/bbTest_ThreadedQueue.c
+++ test/bbTest_ThreadedQueue.c
@@ -7,50 +7,49 @@ thread_local char* thread;
 
 U64 test_time = 0;
 
+bbThreadedQueue queue;
+
 typedef struct
 {
-
-
     bbListElement_Handle list_element;
 } bbTest_Struct;
 
+
+void* consumer_thread(void* arg)
+{
+
+    thread = "CONSUMER THREAD";
+
+
+    while (1)
+    {bbHere()
+        bbTest_Struct* test_struct;
+
+        bbThreadedQueue_popR_block(&queue, (void**)&test_struct);
+
+        bbThreadedQueue_free(&queue, (void**)&test_struct);
+    }
+}
+
 int main (void)
 {
-    bbThreadedQueue queue;
+
+    thread = "PRODUCER THREAD";
+
 
     bbFlag flag = bbThreadedQueue_init(&queue, NULL, sizeof(bbTest_Struct), 7, offsetof(bbTest_Struct, list_element));
 
-    for (I32 i=0; i<7; i++)
-    {
+
+
+    pthread_t consumer_pthread;
+    pthread_create(&consumer_pthread, NULL, consumer_thread, NULL);
+
+    while (1)
+    {bbHere()
         bbTest_Struct* test_struct;
 
         bbThreadedQueue_alloc(&queue, (void**)&test_struct);
 
         bbThreadedQueue_pushL(&queue, test_struct);
-    }
-    for (I32 i=0; i<7; i++)
-    {
-        bbTest_Struct* test_struct;
-
-
-        bbThreadedQueue_popR(&queue, (void**)&test_struct);
-
-        bbThreadedQueue_free(&queue, (void**)&test_struct);
-    }
-    for (I32 i=0; i<7; i++)
-    {
-        bbTest_Struct* test_struct;
-
-        bbThreadedQueue_alloc(&queue, (void**)&test_struct);
-
-        bbThreadedQueue_pushL(&queue, test_struct);
-    }
-    for (I32 i=0; i<7; i++)
-    {
-        bbTest_Struct* test_struct;
-
-
-        bbThreadedQueue_popR(&queue, (void**)&test_struct);
-        bbThreadedQueue_free(&queue, (void**)&test_struct);
     }
 }
