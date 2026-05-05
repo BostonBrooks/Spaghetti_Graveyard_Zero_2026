d38776bec02678ca1c5fe6b4f23df07c2ed2dc8b
boston-brooks <pbrooks193@gmail.com>
2026-05-05 16:42:55 +1000

    cant have a memory leak in the clock thread "USER INTERFACE" if the user interface doesnt use the clock!

--- test/bbTest_SkellyChase.c
+++ test/bbTest_SkellyChase.c
@@ -227,11 +227,11 @@ void* userinterface_thread(void* arg)
         if (home.clock2.is_running){
             if (home.UI.clock2_handle.clock_thread_index == 255)
             {
-                bbClock_handle_init(&home.clock2, &home.UI.clock2_handle, 1,"USER INTERFACE");
+                bbClock_handle_init(&home.clock2, &home.UI.clock2_handle, 0,"USER INTERFACE");
             }
             //may  not need this line:
             //bbClock_waitTick(&home.clock2,&home.UI.clock2_handle,home.UI.clock2_handle.map_tick+1);
-            bbClock_waitTick(&home.clock2,&home.UI.clock2_handle,0);
+            //bbClock_waitTick(&home.clock2,&home.UI.clock2_handle,0);
             home.UI.clock2_handle.map_tick = home.clock2.map_tick;
 
             test_time = home.UI.clock2_handle.map_tick;
