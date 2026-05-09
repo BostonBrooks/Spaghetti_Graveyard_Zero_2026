a34175e8822ffd6452ed5884c27ca7d1b33d6241
boston-brooks <pbrooks193@gmail.com>
2026-05-09 15:18:56 +1000

    separating screen coords from viewport coords

--- engine/geometry/bbGroundCoords.c
+++ engine/geometry/bbGroundCoords.c
@@ -33,6 +33,64 @@ bbFlag matrix_multiply (float A[4][4], float B[4][4], float C[4][4]){
     return bbSuccess;
 }
 
+float signed_area (bbViewportPoints p1, bbViewportPoints p2, bbViewportPoints p3) {
+    //positive is clockwise
+
+    float x1, y1, x2, y2;
+
+    x1 = p2.x - p1.x;
+    y1 = p2.y - p1.y;
+
+    x2 = p3.x - p2.x;
+    y2 = p3.y - p2.y;
+
+    float area = (x1 * y2 - x2 * y1) / 2;
+
+    return area;
+}
+
+I32 bbViewportPoints_withinTriangle_screen (bbViewportPoints point,
+    bbViewportPoints vertex1, bbViewportPoints vertex2, bbViewportPoints vertex3){
+
+    float area = signed_area (vertex1, vertex2, vertex3);
+
+    if (area == 0) {
+
+        return 0;
+    }
+
+    //if (vertex1, vertex2, vertex3) are clockwise and point is within (vertex1, vertex2, vertex3),
+    // (vertex1, vertex2, point) will be clockwise
+    // (vertex2, vertex3, point) will be clockwise
+    // (xertex3, vertex1, point) will be clockwise
+
+
+    //if (vertex1, vertex2, vertex3) are counter-clockwise and point is within (vertex1, vertex2, vertex3),
+    // (vertex1, vertex2, point) will be counter-clockwise
+    // (vertex2, vertex3, point) will be counter-clockwise
+    // (xertex3, vertex1, point) will be counter-clockwise
+
+
+    float trig1 = signed_area (vertex1, vertex2, point);
+    float trig2 = signed_area (vertex2, vertex3, point);
+    float trig3 = signed_area (vertex3, vertex1, point);
+
+    if (trig1 * area >= 0 && trig2 * area >= 0 && trig3 * area >= 0) {
+        return 1;
+    }
+
+    return 0;
+}
+
+I32 bbScreenCoords_withinTriangle_map (bbScreenPoints point,
+    bbMapCoords vertex1, bbMapCoords vertex2, bbMapCoords vertex3)
+{
+
+}
+
+
+
+
 
 
 
