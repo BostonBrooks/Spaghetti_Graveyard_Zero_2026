4a8e555cab1d435c32451c6de05ed8ef960633ff
boston-brooks <pbrooks193@gmail.com>
2026-07-23 08:51:31 +1000

    function declarations in spawn_unit.h

---
+++ engine/ECS/spawn_unit.h
@@ -0,0 +1,28 @@
+#include "engine/logic/bbHandle.h"
+bbFlag bbCoreInbox_TestClick(bbCore* core, bbMapCoords mapCoords);
+bbFlag bbCoreInbox_testClick_fn(bbCore* core, bbLocalMessage* message);
+
+bbFlag bbCoreInput_spawnEntityOut(bbCore* core,
+                                  bbMapCoords MC,
+                                  U64 time,
+                                  bbInstruction_source source,
+                                  bbHandle action);
+
+bbFlag bbInstruction_spawnEntityOut_fn(bbCore* core, bbInstruction* instruction);
+
+bbFlag bbNetworkApp_spawnEntityOut(bbNetwork* network, bbMapCoords MC, U64 time, U32 collision);
+
+
+bbFlag bbAction_spawnUnit(void* Core,
+                            bbMapCoords map_coords,
+                            bbHandle server_entity,
+                            U32 collision,
+                            U64 act_tick);
+
+bbFlag bbCoreInput_spawnTestEntity(bbCore* core,
+                                   bbMapCoords MC,
+                                   bbHandle server_entity,
+                                   bbInstruction_source source,
+                                   bbHandle action);
+
+bbFlag bbInstruction_spawnTestEntity(bbCore* core, bbInstruction* instruction);
