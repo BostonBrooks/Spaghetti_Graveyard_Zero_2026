4ff1aa9bebf77142809f3bf2b660dbf1af130d0e
Boston_Brooks <pbrooks193@gmail.com>
2026-07-27 16:52:55 +1000

    null parse function works

--- games/game0/maps/skellychase/entity_spawner/spawner.c
+++ games/game0/maps/skellychase/entity_spawner/spawner.c
@@ -10,10 +10,32 @@ bbFlag bbPF_null(void* spawner, char* string)
     return bbSuccess;
 }
 
+bbFlag bbPF_simple(void* spawner, char* string)
+{
+    //TODO parse one line of .csv file
+    return bbSuccess;
+}
+
+bbFlag bbSF_null(void* spawner,
+                               bbMapCoords position,
+                               bbMapCoords goalpoint,
+                               bbHandle server_handle,
+                               bbInstruction_source source)
+{
+    bbDebug("position = (%d, %d, %d)\n"
+            "goalpoint = (%d, %d, %d)\n"
+            "server_handle = (%d, %d)\n"
+            "source = %d\n",
+            position.i, position.j, position.k,
+            goalpoint.i, goalpoint.j, goalpoint.k,
+            server_handle.bloated.index, server_handle.bloated.collision,
+            source);
+}
 
 bbFlag bbEntitySpawner_populate(bbEntitySpawner* spawner)
 {
     bbParseFunction_add(spawner, bbPF_null, "NULL");
 
+    bbSpawnFunction_add(spawner, bbSF_null, "NULL");
     return bbSuccess;
 }
