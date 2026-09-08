47b4c9e8f958a91ae5bd25e9da2eaa7371a6e642
Boston_Brooks <pbrooks193@gmail.com>
2026-09-08 19:13:46 +1000

    comments on all bbFlag types

--- engine/logic/bbFlag.h
+++ engine/logic/bbFlag.h
@@ -11,22 +11,22 @@
 // the vast majority of functions return bbFlag, and other values are
 // returned by reference.
 typedef enum {
-    bbSuccess,
-    bbFail,
-    bbNone,
-    bbFull,
-    bbEmpty,
-    bbBreak,
-    bbRepeat,
-    bbContinue,
-    bbDelete,
-    bbHead,
-    bbTail,
-    bbHandleError_System,
-    bbHandleError_Index,
-    bbHandleError_Generation,
-    bbHandleError_Stale,
-    bbHandleError_NULL,
+    bbSuccess, //Generic pass
+    bbFail, //Generic fail
+    bbNone, //Looking handle not found in pool/lookup table
+    bbFull, //Cannot allocate a new element in pool
+    bbEmpty, //Getting an element from an empty list
+    bbBreak, //Stop mapping the function to the list
+    bbRepeat, //Map the function again to the same element of the list
+    bbContinue, //Map the function to the next element of the list
+    bbDelete, //delete the current entry when mapping a function to a list
+    bbHead, //First element of the list
+    bbTail, //last element of the list
+    bbHandleError_System, //Handle belongs to wrong system
+    bbHandleError_Index,  //For when you compare two handles and their indices mismatch
+    bbHandleError_Generation, //The handle is stale?
+    bbHandleError_Stale, //The handle is stale?
+    bbHandleError_NULL, //Using a NULL handle
 
 } bbFlag;
 
