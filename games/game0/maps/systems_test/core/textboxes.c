bade0169400bded26f7f3fd32293721709bb8919
boston <pbrooks193@gmail.com>
2026-09-13 12:02:12 +1000

    bbCs_putTextbox() - put a message to the textbox

--- games/game0/maps/systems_test/core/textboxes.c
+++ games/game0/maps/systems_test/core/textboxes.c
@@ -41,3 +41,28 @@ bbFlag bbCD_unsetTextbox_fn(bbCore* core, bbInstruction* undo_instruction) {
     bbNotImplemented()
     return bbSuccess;
 }
+
+
+bbFlag bbCI_putTextbox(bbCore* core, char* string, char* key, U64 time, bbInstruction_source source, bbHandle action) {
+    bbNotImplemented()
+    return bbSuccess;
+}
+bbFlag bbCS_putTextbox(bbCore* core, char* string, char* key, U64 time, bbInstruction_source source, bbHandle action)
+{
+    bbNotImplemented(); //spawn undo instruction
+
+    bbTextbox* textbox;
+    bbHandle textbox_handle;
+    bbDictionary_lookup(home.textbox_system.dict,key,&textbox_handle);
+    textbox = textbox_handle.ptr;
+
+    bbHandle message_handle;
+    char* message_text;
+    bbTextbox_newMessage(textbox,&message_handle,&message_text);
+
+    snprintf(message_text,MESSAGE_LENGTH,"%s",string);
+    bbTextbox_putMessage(textbox,message_handle, time);
+    bbTextbox_updateBuffer(textbox);
+
+    return bbSuccess;
+}
--- games/game0/maps/systems_test/core/textboxes.h
+++ games/game0/maps/systems_test/core/textboxes.h
@@ -5,4 +5,8 @@ bbFlag bbI_unsetTextbox_fn(bbCore* core, bbInstruction* instruction);
 bbFlag bbCI_setTextbox(bbCore* core, char* string, char* key, U64 time, bbInstruction_source source, bbHandle action);
 bbFlag bbCS_setTextbox(bbCore* core, char* string, char* key, U64 time, bbInstruction_source source, bbHandle action);
 
+
+bbFlag bbCI_putTextbox(bbCore* core, char* string, char* key, U64 time, bbInstruction_source source, bbHandle action);
+bbFlag bbCS_putTextbox(bbCore* core, char* string, char* key, U64 time, bbInstruction_source source, bbHandle action);
+
 bbFlag bbCD_unsetTextbox_fn(bbCore* core, bbInstruction* undo_instruction);
